36d745796dbb93f50cdfbe31ead774a388b2520f
mithrayls <mithray@protonmail.com>
2019-09-16 04:29:41 +1200

    refactor include interactive mode

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 'use strict'
 const fs = require('fs')
 const yargs = require('yargs-parser')
-const Joi = require('@hapi/joi')
-const Hapi = require('@hapi/hapi')
-const HapiSwagger = require('hapi-swagger')
-const Inert = require('@hapi/inert')
-const Vision = require('@hapi/vision')
 const fetch = require('node-fetch')
-
-const testFunction = require('./lib/testFunction.js')
 const queryEncode = require('./lib/queryEncode.js')
+const puppi = require('./lib/puppi.js')
+const interactive = require('./lib/interactive.js')
+const startServer = require('./lib/startServer.js')
 //const packageJson = JSON.parse(fs.readFileSync('./package.json'))
 
 let domain = 'localhost'
@@ -18,80 +14,40 @@ let delimiter = '://'
 let port = '19271'
 
 async function emiln(routes, argv) {
+    //console.log(routes)
     const args = yargs(process.argv.slice(2))
-    if (args.daemon) {
-        console.log('daemon detected')
-    }
     const queryString = queryEncode(args)
-    const server = Hapi.server({
-        port: port,
-        host: domain
-    })
-    for (let routeFamily of routes) {
-        routes = Object.values(routeFamily)
-        for (let route of routes) {
-            if (route.options && route.options.validate) {
-                let validation = route.options.validate
-                for (let key in validation) {
-                    for (let key2 in route.options.validate[key]) {
-                        route.options.validate[key][key2] = eval(
-                            route.options.validate[key][key2]
-                        )
-                    }
-                }
-            }
-            server.route(route)
-        }
-    }
-    process.on('unhandledRejection', err => {
-        console.log(err)
-        process.exit(1)
-    })
-    await server.start()
     const endpoint = protocol + delimiter + domain + ':' + port
     const uri = endpoint + queryString
-
+    let server = await startServer({ routes, domain, port })
     if (args.daemon) {
-        console.log('server listening on ' + endpoint)
+        console.log('Server listening on ' + endpoint)
     }
-    /*
-    const swaggerOptions = {
-        info: {
-            title: packageJson.name,
-            version: packageJson.version
-        }
-    }
-
-    await server.register([
-        Inert,
-        Vision,
-        {
-            options: swaggerOptions,
-            plugin: HapiSwagger
-        }
-    ])
-
-
-    console.log('Fetching swagger.json: ' + endpoint + '/swagger.json')
-    let swagger = await fetch(endpoint + '/swagger.json').then(async res => {
-        let swagger_json = await res.text()
-        fs.writeFileSync('./swagger.json', swagger_json)
-        return swagger_json
-    })
-*/
 
     console.log('Calling API: ' + uri)
-    let res = await fetch(uri).then(async res => {
-        let response = await res.text()
-        console.log(response)
-        return response
-    })
+    let res = await puppi(uri)
+
+    if (args.interactive) {
+        args.daemon = true
+        interactive({
+            routes: require('../../sempub/spec/git.js'),
+            endpoint: endpoint
+        })
+    }
 
     if (!args.daemon) {
         server.stop({ timeout: 60 * 1000 })
     }
-
-    return res
 }
 
 module.exports = emiln
+
+/*
+ *
+
+module.exports = { registerStorage }
+
+*/
+let routes = require('../sempub/spec/git.js')
+
+emiln([routes], ['a', 'b', 'c', 'd'])
